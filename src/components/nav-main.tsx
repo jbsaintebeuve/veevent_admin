2057a138a789b1cb8033a28ee419a44dcd9da35f
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-09-09 17:18:31 +0200

    refactor: add isAdmin prop to CreateEventDialog for conditional rendering based on user role

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 import { CreateEventDialog } from "./create-dialogs/create-event-dialog";
+import { useAuth } from "@/hooks/use-auth";
 
 export function NavMain({
   items,
@@ -25,6 +26,11 @@ export function NavMain({
 }) {
   const pathname = usePathname();
   const { setOpenMobile } = useSidebar();
+  const { user } = useAuth();
+
+  const isAdmin =
+    user?.role.toLowerCase() === "admin" ||
+    user?.role.toLowerCase() === "authservice";
 
   const handleLinkClick = () => {
     setOpenMobile(false);
@@ -42,7 +48,7 @@ export function NavMain({
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <CreateEventDialog>
+            <CreateEventDialog isAdmin={isAdmin}>
               <SidebarMenuButton className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear">
                 <PlusCircle className="h-4 w-4" />
                 <span>Créer un événement</span>
